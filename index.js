5301505cd1d85518af579d24797a7fbe9e3d93b0
2547techno <technowizz2547@gmail.com>
2023-02-26 02:12:03 -0500

    update log

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.get("/", (req, res) => {
 app.get("/:username", (req, res) => {
     if (req.params.username.startsWith("favicon.")) return;
 
-    console.log(`${new Date()} | ${req.params.username}`);
+    console.log(`${new Date()} | /${req.params.username}`);
     res.sendFile(__dirname + "/username.html")
 })
 
 app.get("*", (req, res) => {
+    console.log(`${new Date()} | /`);
     res.sendFile(__dirname + "/404.html")
 })
 
